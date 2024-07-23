6391bbf4d1c1325301f1a20a28c58cb189bf059c
josc146 <josStorer@outlook.com>
2024-07-23 14:33:39 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Text.Json;
 using System.Threading;
@@ -43,13 +43,15 @@ internal class Program
         if (gpu == null)
             gpu = computer.Hardware.FirstOrDefault(h =>
                 h.HardwareType == HardwareType.GpuAmd || h.HardwareType == HardwareType.GpuIntel);
+        var gpuName = gpu?.Name;
+        var gpuType = gpu?.HardwareType.ToString().Replace("Gpu", "");
 
         while (true)
         {
             computer.Accept(new UpdateVisitor());
 
             float usedMemory = -1, availableMemory = -1, totalMemory = -1;
-            float gpuUsage = -1, gpuPower = -1, usedVram = -1, totalVram = -1;
+            float gpuUsage = -1, usedVram = -1, totalVram = -1;
 
             if (memory != null)
                 foreach (var s in memory.Sensors)
@@ -70,27 +72,23 @@ internal class Program
                 {
                     if (s.Name == "GPU Core" && s.SensorType == SensorType.Load)
                         gpuUsage = s.Value.GetValueOrDefault();
-                    else if (s.Name == "GPU Package")
-                        gpuPower = s.Value.GetValueOrDefault();
                     else if (s.Name == "GPU Memory Used")
                         usedVram = s.Value.GetValueOrDefault();
                     else if (s.Name == "GPU Memory Total")
                         totalVram = s.Value.GetValueOrDefault();
-                    if (gpuUsage != -1 && gpuPower != -1 && usedVram != -1 && totalVram != -1) break;
+                    if (gpuUsage != -1 && usedVram != -1 && totalVram != -1) break;
                 }
 
-            var data = new
+            Console.WriteLine(JsonSerializer.Serialize(new
             {
+                gpuType,
+                gpuName,
                 usedMemory,
                 totalMemory,
                 gpuUsage,
-                gpuPower,
                 usedVram,
                 totalVram
-            };
-
-            var jsonString = JsonSerializer.Serialize(data);
-            Console.WriteLine(jsonString);
+            }));
 
             Thread.Sleep(750);
         }
